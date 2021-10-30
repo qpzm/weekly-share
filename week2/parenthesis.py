428bbc7714897ebaad1b40e022d43094c237fb67
Hyunmin Lee <hyunmin@toss.im>
2021-10-30 18:53:25 +0900

    Week2-1. 괄호 매칭

---
+++ week2/parenthesis.py
@@ -0,0 +1,25 @@
+def main():
+    N = int(input())
+    for _ in range(N):
+        print(format(check(str(input()))))
+
+def check(str):
+    stack = []
+    for s in str:
+        if s == '(':
+            stack.append(s)
+        elif(len(stack) != 0):
+            stack.pop()
+        else:
+            return False
+
+    return len(stack) == 0
+
+def format(x):
+    if x:
+        return 'YES'
+    else:
+        return 'NO'
+
+
+main()
